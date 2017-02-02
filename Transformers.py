6f01f78cf26e073ab9236e962174677614556ce5
Vitor Costa <vitorpdcosta@gmail.com>
2017-02-02 01:24:30 -0200

    2

--- Transformers.py
+++ Transformers.py
@@ -4,26 +4,47 @@
 #importing the matemathical library
 import math
 import matplotlib.pyplot as plt
+import numpy as np
 
+# Write the name of the file
 filename = 'file-800.txt'
+
+
+Hcte = 355.8676647
+Bcte = 0.501564416
+
 time = []
 current = []
 voltage = []
+Hcamp = []
+Bcamp = []
+
+
+# reading the channels
+def DocumentRead():
+    for n,line in enumerate(data, 1):
+    #    print ('{:2}.'.format(n), line.rstrip())
+        parts = line.split(',')
+        time.append(parts[:1])
+        current.append(parts[1:2])
+        voltage.append(parts[2:3])
+
+
 #opening the data files
 with open(filename) as f:
     data = f.readlines()
 
-# reading the channels
-for n,line in enumerate(data, 1):
-    print ('{:2}.'.format(n), line.rstrip())
-    parts = line.split(',')
-    time.append(parts[:1])
-    current.append(parts[1:2])
-    voltage.append(parts[2:3])
-print(time)
-print(current)
-print(voltage)
+#main Program
+
+DocumentRead()
+
+
+Hcamp= np.array(current)
+Bcamp = np.array(voltage)
+
+print(Hcamp)
+print(type(Hcamp))
 
 # printing the graphics
-plt.plot(time,current)
+plt.plot(Hcamp,Bcamp)
 plt.show()
