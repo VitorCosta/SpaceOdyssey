01d18f3a2f2321e321070660b2dfbc25891889b3
Vitor Costa <vitorpdcosta@gmail.com>
2017-02-03 01:33:27 -0200

    potency add

--- Transformers.py
+++ Transformers.py
@@ -4,32 +4,38 @@
 #importing the matemathical library
 import math
 import matplotlib.pyplot as plt
-import numpy as np
+from numpy import mean, sqrt, square, arange
 
-# Write the name of the file
+
+# Write the name of the file5
 filename = 'file-800.txt'
 
 
-Hcte = 355.8676647
-Bcte = 0.501564416
 
 time = []
 current = []
 voltage = []
 Hcamp = []
 Bcamp = []
+Pot = []
+Hcte = 355.8676647
+Bcte = 0.501564416
 
 
-# reading the channels
+# reading the channels and access the string item, get the first element and append to an array
 def DocumentRead():
     for n,line in enumerate(data, 1):
-    #    print ('{:2}.'.format(n), line.rstrip())
+        #print ('{:2}.'.format(n), line.rstrip())
         parts = line.split(',')
-        time.append(parts[:1])
-        current.append(parts[1:2])
-        voltage.append(parts[2:3])
+        time.append(parts[:1][0])
+        current.append(float(parts[1:2][0]))
+        voltage.append(float(parts[2:3][0]))
+        Hcamp.append(float(parts[1:2][0]) * Hcte)
+        Bcamp.append(float(parts[2:3][0]) * Bcte)
 
-def Plotcurrentvoltage():
+
+#plot the read current and voltage
+def PlotCurrentVoltage():
     plt.plot(time,current, label='Current')
     plt.plot(time,voltage, label='Voltage')
     plt.xlabel('Time')
@@ -37,8 +43,26 @@ def Plotcurrentvoltage():
     plt.title('Timex.current.voltage')
     plt.grid()
     plt.legend()
-    plt.show()
 
+#Plot the potency graphic
+def PlotPotency():
+    Pot = [current*voltage for current,voltage in zip(current,voltage)]
+    plt.plot(time,Pot)
+    plt.xlabel('time')
+    plt.ylabel('Amplitude[W]')
+    plt.title('PotxTime')
+    Med = float(sum(Pot)/len(Pot))
+    Rms = float(sqrt(mean(square(Pot))))
+    print(Med)
+    print(Rms)
+
+
+#Plot the magnetization curve
+def PlotCamps():
+    plt.plot(Hcamp,Bcamp)
+    plt.xlabel('A/M')
+    plt.ylabel('Tesla')
+    plt.title('HxB')
 
 #opening the data files
 with open(filename) as f:
@@ -47,14 +71,6 @@ with open(filename) as f:
 #main Program
 
 DocumentRead()
-Plotcurrentvoltage()
-
-Hcamp= np.array(current)
-Bcamp = np.array(voltage)
-
-print(Hcamp)
-print(type(Hcamp))
-
-# printing the graphics
-#plt.plot(Hcamp,Bcamp)
-#plt.show()
+PlotCurrentVoltage()
+PlotPotency()
+PlotCamps()
