b4905c3a766229deafba5401c390fa84217e8784
Vitor Costa <vitorpdcosta@gmail.com>
2017-02-18 22:14:51 -0200

    fft working

--- Transformers.py
+++ Transformers.py
@@ -15,8 +15,8 @@ voltage = []
 Hcamp = []
 Bcamp = []
 Pot = []
-Hcte = 355.8676647
-Bcte = 0.501564416
+Hcte = 285.93
+Bcte = 0.8705483323911815
 fourrier = []
 
 
@@ -31,31 +31,40 @@ def DocumentRead(x):
 
 #plot the read current and voltage
 def PlotCurrentVoltage():
-    GraphCV = plt.figure()
-    axes = GraphCV.add_axes([0.1,0.1,0.8,0.8])
-    axes.plot(time,current,color = 'red',label='Current')
-    axes.plot(time,voltage,color = 'black',label='Voltage')
-    axes.set_title('Time versus Current and Voltage')
-    axes.set_xlabel('Amplitude')
-    axes.set_ylabel('Time')
-    axes.grid(color='b', alpha=0.5, linestyle='dashed', linewidth=0.5)
-    axes.legend(loc=(1.01,1.01))
+    fig = plt.figure()
+
+    axes1 = fig.add_axes([0.1,0.1,0.5,0.5])
+    axes2 = fig.add_axes([0.88,0.1,0.5,0.5])
+    axes1.plot(time,current,color = 'red',label='Current')
+    axes1.set_title('Time versus Current')
+    axes1.set_ylabel('Amplitude')
+    axes1.set_xlabel('Time')
+    axes1.grid(color='b', alpha=0.5, linestyle='dashed', linewidth=0.5)
+    axes2.plot(time,voltage,color = 'black',label='Voltage')
+    axes1.legend(loc=0)
+    axes2.set_xlabel('Time')
+    axes2.set_ylabel('Amplitude')
+    axes2.grid(color='b',alpha=0.5,linestyle='dashed',lw=0.5)
+    axes2.set_title('Voltage versus Time')
+    axes2.legend(loc=0)
     CurrentRms = np.sqrt(np.mean(np.square(current)))
     VoltageRms = np.sqrt(np.mean(np.square(voltage)))
     print('The Rms Current is: {}'.format(CurrentRms))
     print('The Rms Voltage is: {}'.format(VoltageRms))
-    plt.show(GraphCV)
+    plt.show(fig)
 
 #lot the potency graphic
 def PlotPotency():
     Pot = voltage * current
-    plt.plot(time,Pot)
-    plt.xlabel('time')
-    plt.ylabel('Amplitude[W]')
-    plt.title('PotxTime')
-    plt.grid()
-    Med = float(np.sum(Pot)/len(Pot))
-    Rms = float(np.sqrt(np.mean(np.square(Pot))))
+    fig = plt.figure()
+    axes = fig.add_axes([0,0,2.0,1])
+    axes.plot(time,Pot,'purple')
+    axes.set_xlabel('Time')
+    axes.set_ylabel('Potency[W]')
+    axes.set_title('Pot versus Time')
+    axes.grid()
+    Med = float(np.absolute(np.mean(Pot)))/2
+    Rms = float(np.sqrt(np.mean(np.square(Pot))))/2
     print('The Med Value is: {}'.format(Med))
     print('The Rms Value is: {}'.format(Rms))
     plt.show()
@@ -63,15 +72,18 @@ def PlotPotency():
 
 #Plot the magnetization curve
 def PlotCamps():
-    plt.plot(Hcamp,Bcamp)
+    fig = plt.figure()
+    axes = fig.add_axes([0,0,1,1])
+    axes.plot(Hcamp,Bcamp,'grey')
     plt.xlabel('A/M')
     plt.ylabel('Tesla')
     plt.title('HxB')
     plt.grid()
     plt.show()
-    Energy = np.trapz(Bcamp,Hcamp,dx=0.01,axis=0)
+    Energy = np.trapz(Bcamp,Hcamp,dx=0.1,axis=0)
     print('The Energy Value is: {}'.format(abs(Energy)))
-    vol=0.000142738;
+    #vol=0.000142738;
+    vol = 0.0018559999999999996
     Pfe = abs(Energy) * vol * 60
     print('The Potency Value is: {}'.format(abs(Pfe)))
 
@@ -80,33 +92,44 @@ def Fft():
     L = len(sample)
     print(L)
     Y = scipy.fftpack.fft(sample)/L
-    #print(Y)
     xdft = Y[0:L/2+1]
     print(len(xdft))
     freq = np.linspace(0,L/2,L/2+1)
     print(len(freq))
     ampl_fft = 2 * np.absolute(xdft)
-    #print (ampl_fft)
+    FFTfigure = plt.figure()
+    axes = FFTfigure.add_axes([0,0,2.0,1.0])
     plt.bar(freq,ampl_fft)
     plt.axis([0,50,0,1.5])
+    axes.set_xticks(np.arange(0,51,1))
     plt.grid()
     plt.show()
-    print(freq)
+    x = 0
+    while x<=10:
+        print('Fft amplitude:{} - {}'.format(x,ampl_fft[x]))
+        x = x + 1
+    x  = 2
+    somv = 0
+    while x<=50:
+        somv = somv + (np.absolute(ampl_fft[x]**2))
+        x = x + 1
+    THD = 100 * np.sqrt(somv)/(np.absolute(ampl_fft[1]))
+    print('THD: {} '.format(THD))
 
 
 
 #main Program
 
 
-df = DocumentRead('60Hz800.csv')
+h = 210
+df = DocumentRead('TEK.csv')
+time = df['TIME'][:h]
+current = df['CH1'][:h]
+voltage = df['CH3'][:h] * 4
+Hcamp = df['CH1'][:h] * Hcte
+Bcamp = df['CH2'][:h] * Bcte
 
-time = df['TIME']
-current = df['CH1']
-voltage = df['CH2']
-Hcamp = df['CH1'] * Hcte
-Bcamp = df['CH2'] * Bcte
-
-#Fft()
+Fft()
 PlotCurrentVoltage()
 PlotPotency()
 PlotCamps()
